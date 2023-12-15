38983f0864a04f907ba962f8fa34d41739776ee2
RafaxWolf <78835495+RafaxWolf@users.noreply.github.com>
2023-12-15 00:54:58 -0300

    Delete Schema directory

--- Schema/inventory-schema.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model} = require("mongoose")
-
-const inventory = new Schema({
-    userID: String,
-    guildID: String,
-    Inventory: Object,
-});
-
-module.exports = model('inventory', inventory)
