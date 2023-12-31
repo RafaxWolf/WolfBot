8b226faf089c38c6ca1e5baebb6cd8783cb559e1
RafaxWolf <rafalisandroflores@gmail.com>
2023-12-31 00:53:25 -0300

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
+const chalk = require("chalk")
 
 mongoose.connect('mongodb+srv://WolfBot:xuxo2006@cluster0.vrs4i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(db => console.log("conectado a MongoDB"))
+.then(db => console.log("Conectado a " + chalk.greenBright("MongoDB")))
 .catch(err => console.log(err))
