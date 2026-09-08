44bda66e6b81bd6c054c52517ce90e8a22cff50f
RafaxWolf <rafalisandroflores@gmail.com>
2026-09-08 02:35:32 -0300

    New WolfBot Update

--- src/config.js
+++ src/config.js
@@ -1,23 +1,32 @@
+const { TOKEN, CLIENTID, GUILD, VERIFICATION_CHANNEL, MONGODB_USER, MONGODB_PASSWD, GENIUS_API_KEY, TERMS_CHANNEL } = process.env;
+
+const chalk = require("chalk");
 require("dotenv").config({ quiet: true });
 
-const { TOKEN, CLIENTID, GUILD, VERIFICATION_CHANNEL, MONGODB_USER, MONGODB_PASSWD, GENIUS_API_KEY } = process.env;
-
-if ( !TOKEN || !CLIENTID ) {
-    console.error("❌ | Missing required environment variables. Please check your .env file.");
+//! Validación de credenciales de Discord
+if (!TOKEN || !CLIENTID) {
+    console.error(chalk.red("❌ | Token o Client ID Faltantes. Por favor, revisa tu archivo .env y asegúrate de que las variables TOKEN y CLIENTID estén definidas."));
     process.exit(1);
 }
 
-if ( !MONGODB_USER || !MONGODB_PASSWD ) {
-    console.error("❌ | Missing MongoDB credentials in environment variables. Please check your .env file.");
+//! Validación de credenciales de MongoDB
+if (!MONGODB_USER || !MONGODB_PASSWD) {
+    console.error(chalk.red("❌ | Credenciales de MongoDB faltantes. Por favor, revisa tu archivo .env y asegúrate de que las variables MONGODB_USER y MONGODB_PASSWD estén definidas."));
     process.exit(1);
+}
+
+//? Si el canal de verificación no está definido en el archivo .env, se asigna un valor predeterminado y se muestra una advertencia en la consola.
+if(!VERIFICATION_CHANNEL) {
+    VERIFICATION_CHANNEL = "1063533274233852005"; // Valor predeterminado si no se encuentra en el archivo .env
+    console.warn(chalk.yellow("⚠️ | La variable VERIFICATION_CHANNEL no está definida en el archivo .env. Se utilizará el valor predeterminado: 1063533274233852005"));
+}
+
+//? Si el canal de Normas/Terminos no está definido en el archivo .env, se asigna un valor predeterminado y se muestra una advertencia en la consola.
+if (!TERMS_CHANNEL) {
+    TERMS_CHANNEL = "936741059642413107"; // Valor predeterminado si no se encuentra en el archivo .env
+    console.warn(chalk.yellow("⚠️ | La variable TERMS_CHANNEL no está definida en el archivo .env. Se utilizará el valor predeterminado: 936741059642413107"));
 }
 
 module.exports = {
-    TOKEN,
-    CLIENTID,
-    GUILD,
-    VERIFICATION_CHANNEL,
-    MONGODB_USER,
-    MONGODB_PASSWD,
-    GENIUS_API_KEY
+    TOKEN, CLIENTID, GUILD, VERIFICATION_CHANNEL, TERMS_CHANNEL, MONGODB_USER, MONGODB_PASSWD, GENIUS_API_KEY
 };
--- src/events/ready.js
+++ src/events/ready.js
@@ -5,7 +5,7 @@ const path = require("path")
 const fs = require("fs")
 
 const getBasePath = require("../utils/getBasePath");
-const { VERIFICATION_CHANNEL } = require("../config");
+const { VERIFICATION_CHANNEL, TERMS_CHANNEL } = require("../config");
 
 module.exports = {
   name: 'clientReady',
@@ -13,31 +13,35 @@ module.exports = {
   async execute(client) {
 
 //* Sistema de Verificación
-const filePath = path.join(getBasePath(), "events", "verification_message_id.json"); //* Ruta del archivo JSON donde se guarda el ID del mensaje de verificación
+const filePath = path.join(getBasePath(), "events", "verification_id.json"); //* Ruta del archivo JSON donde se guarda el ID del mensaje de verificación
+const channel = client.channels.cache.get(VERIFICATION_CHANNEL)
 
-//? Función para crear y enviar el mensaje de verificación
-/**
- * Creates, sends the verification message to the specified channel and register the message ID.
- * @param {*} channel Channel where the verification message will be sent.
- */
-async function createVerificationMessage(channel) {
-  // Crea el botón de verificación
-  const row = new ActionRowBuilder()
-    .addComponents(
-      new ButtonBuilder()
-      .setCustomId('verification')
-      .setLabel("Verificarse")
-      .setStyle(ButtonStyle.Primary)
-    )
+// Crea el botón de verificación
+const row = new ActionRowBuilder()
+  .addComponents(
+    new ButtonBuilder()
+    .setCustomId('verification')
+    .setLabel("Verificarse")
+    .setStyle(ButtonStyle.Primary)
+  )
 
-  // Crea el embed de verificación
-  const verificationEmbed = new EmbedBuilder() //* Embed de verificación
-    .setAuthor({ name: "The Hidden Wolf's", iconURL: "https://i.imgur.com/xTNwaQR.png" })
-    .setTitle("Verificación")
-    .setDescription("Para verificarte debes pulsar el botón de abajo.\nRecuerda leer las <#936741059642413107> del servidor.")
-    .setColor("Blue")
-    .setFooter({ text: "(Si no puedes verificarte abre un ticket con moderación para poder verificarte)", iconURL: client.user.displayAvatarURL() })
-    
+// Crea el embed de verificación
+const verificationEmbed = new EmbedBuilder() //* Embed de verificación
+  .setAuthor({ name: "The Hidden Wolfs", iconURL: "https://i.imgur.com/xTNwaQR.png" })
+  .setTitle("Verificación")
+  .setDescription(`Para verificarte debes pulsar el botón de abajo.\nRecuerda leer las <#${TERMS_CHANNEL}> del servidor.`)
+  .setColor("Grey")
+  .setFooter({ text: "(Si no puedes verificarte abre un ticket con moderación para poder verificarte)", iconURL: client.user.displayAvatarURL() })
+
+
+  //? Función para crear y enviar el mensaje de verificación
+  /**
+   * Crea y envía un mensaje de verificación al canal especificado. Este mensaje contiene un embed con información sobre la verificación y un botón que los usuarios pueden presionar para verificarse.
+   * El ID del mensaje de verificación se guarda en un archivo JSON para futuras referencias.
+   * @param {*} channel Canal de Discord donde se enviará el mensaje de verificación.
+   */
+  async function createVerificationMessage(channel) {
+
     // Envía el mensaje de verificación al canal
     const reply = await channel.send({ embeds: [verificationEmbed], components: [row] }) //* Envía el embed con el botón para poder verificarse
     const newVerificationMessageID = reply.id
@@ -47,67 +51,54 @@ async function createVerificationMessage(channel) {
     fs.writeFileSync(filePath, JSON.stringify({ verificationMessageID: newVerificationMessageID }))
   }
 
-  // Lee el archivo JSON para obtener el ID del mensaje de verificación
-  try {
 
-    let data = {};
+  //* Lee el archivo JSON para obtener el ID del mensaje de verificación
+    try {
+      let data = {};
+      const { verificationMessageID } = JSON.parse(data)
 
-    if(fs.existsSync(filePath)) {
+      if(!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, JSON.stringify(data))
+        console.log(chalk.blueBright("\n[+] Archivo de verificación creado!\n"))
+      }
+      
       data = fs.readFileSync(filePath, 'utf-8')
       console.log(chalk.greenBright("\n[+] Archivo de verificación encontrado!\n"))
 
-    } else {
-      fs.writeFileSync(filePath, JSON.stringify(data))
-      console.log(chalk.blueBright("\n[+] Archivo de verificación creado!\n"))
-
-    }
-
-    //*----- Canal de verificación e ID del mensaje -----
-    const channel = client.channels.cache.get(VERIFICATION_CHANNEL)
-    const { verificationMessageID } = JSON.parse(data)
-    //*--------------------------------------------------
-
-    if(channel) {
+      if(!channel) {
+        console.log(chalk.red("[!] No se pudo encontrar el canal de verificación!\n"))
+      }
 
       //* Si existe el ID del mensaje de verificación, verifica si el mensaje sigue existiendo
-      if(verificationMessageID) {
-        try{
-          const existingMessage = await channel.messages.fetch(verificationMessageID);
-
-          // Si el mensaje existe, no hace nada
-          if(existingMessage) {
-            console.log(chalk.greenBright("[+] El mensaje de verificación existe!\n"))
-            
-          // Si el mensaje no existe, envía uno nuevo
-          } else {
-            console.log(chalk.redBright("[!] El embed de Verificación ya no existe o no se encuentra!\n"))
-            fs.writeFileSync(filePath, JSON.stringify({ verificationMessageID: null })) //* Elimina el ID del embed anterior
-            console.log(chalk.blueBright("[-] ID del embed anterior eliminado. enviando nuevo mensaje de verificación...\n"))
-    
-            createVerificationMessage(channel)
-          }
-        
-        // Si ocurre un error, pero el error no es 10008 (mensaje no encontrado), muestra el error
-        } catch (error) {
-          if (!error.code === 10008) {
-          console.log(chalk.redBright(`[!] Ha ocurrido un error:\n`), error)
-          }
-        }
-    
-      // Si no existe el mensaje de verificación, envía uno nuevo
-      } else {
+      if(!verificationMessageID) {
         createVerificationMessage(channel)
       }
 
-    // Si no se encuentra el canal de verificación, muestra un error
-    } else {
-      console.log(chalk.red("[!] No se pudo encontrar el canal de verificación!\n"))
-    }
+      try {
+        const existingMessage = await channel.messages.fetch(verificationMessageID);
 
-  // Si ocurre un error al leer o escribir el archivo JSON, muestra el error
-  } catch (err) {
-    console.error(chalk.redBright("[!] Error al Leer/Escribir al archivo\n"), err)
-  }
+        //* Si el mensaje no existe, lo crea de nuevo y elimina el ID del mensaje anterior del archivo JSON
+        if(!existingMessage) {
+          console.log(chalk.redBright("[!] El embed de Verificación ya no existe o no se encuentra!\n"))
+          fs.writeFileSync(filePath, JSON.stringify({ verificationMessageID: null })) //* Elimina el ID del embed anterior
+          console.log(chalk.blueBright("[-] ID del embed anterior eliminado. enviando nuevo mensaje de verificación...\n"))
+
+          createVerificationMessage(channel)
+        }
+
+        console.log(chalk.greenBright("[+] El mensaje de verificación existe!\n"))
+
+      //? Si ocurre un error, pero el error no es 10008 (mensaje no encontrado), muestra el error
+      } catch (error) {   
+        if (!error.code === 10008) {
+        console.error(chalk.redBright(`[!] Ha ocurrido un error:\n`), error)
+        }
+      }
+
+    //? Si ocurre un error al leer o escribir el archivo JSON, muestra el error
+    } catch (err) {
+      console.error(chalk.redBright("[!] Error al Leer/Escribir al archivo\n"), err)
+    }
 
     //! Encendido del bot
     console.log(chalk.cyanBright(`Logged in as ${client.user.tag}.\n`));
@@ -115,7 +106,7 @@ async function createVerificationMessage(channel) {
       activities: [
         { name: "w!help - /help", type: ActivityType.Playing }
       ], status: "dnd"
-    });
+    })
     
-  },
-};
+  }
+}
